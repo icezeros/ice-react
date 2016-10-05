974377cdfce2f784788ed23c4fb77e348f055d60
guosen hu <guosen hu>
2016-10-06 02:08:38 +0800

    init

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
-import App from './components2/App'
+import App from './components/App'
 // import Home from './components/Home'
 // import Article from './components/Article'
 // import Login from './components/Login'
